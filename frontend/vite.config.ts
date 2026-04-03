788bc4d32c1f03e767b5a443fcff8a6317956781
DARK-1926 <mohitkumar2452007@gmail.com>
2026-04-03 12:35:45 +0530

    fix: hardcode base path for GitHub Pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_ACTIONS ? "/NisargaSoft/" : "/",
+  base: "/NisargaSoft/",
   server: {
     port: 3000,
     host: "0.0.0.0",
